217b9e9ff452da81b12ac94be3d0409538637046
Jodu555 <Jodu505@gmail.com>
2022-03-01 12:29:51 +0100

    Ported everything from the nodecloud to here

---
+++ src/MCServer.js
@@ -0,0 +1,65 @@
+const execute = require('child_process').exec;
+
+const command = 'java -jar spigot.jar';
+class MCServer {
+
+    /**
+     * @param  {String} name the servers referencing name
+     * @param  {String} cwd='./CloudData/spigot-Server'
+     * @param  {Object} serverProperties the server.properties
+     */
+    constructor(name, cwd = './CloudData/spigot-Server', serverProperties = {}) {
+        this.name = name;
+        this.cwd = cwd;
+        this.logs = [];
+    }
+
+    listenForLog() {
+        this.process.stdout.on('data', (data) => {
+            data = data.toString().split(/(\r?\n)/g);
+            data.forEach((item, index) => {
+                var line = data[index].trim();
+                if (line !== '\n' && line !== '') {
+                    // console.log('Console Output: "', data[index].trim(), '"');
+                    this.logs.push(data[index].trim());
+                }
+            });
+        });
+    }
+    /**
+     * @param  {String} command the command
+     */
+    sendCommand(command) {
+        this.process.stdin.setEncoding("utf8");
+        this.process.stdin.write(command + '\n');
+    }
+
+    stop() {
+        this.process.kill('SIGINT');
+    }
+
+    start() {
+        this.process = execute(command, { cwd: this.cwd }, (err, stdout, stderr) => {
+            if (err) {
+                console.error('Server:start Error: ', err);
+                return;
+            }
+            if (stderr) {
+                console.error('Server:start Error: ', stderr);
+                return;
+            }
+            if (stdout) {
+                // console.log('LOG OUTPUT: ', stdout);
+            }
+        });
+
+        try {
+            this.listenForLog();
+        } catch (error) {
+            console.log('Error catched!');
+            console.error(error);
+        }
+    }
+}
+
+module.exports = MCServer;
