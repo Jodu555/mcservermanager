7c3f2f7a4d982e2fee609acff655408a07b1a401
Jodu555 <Jodu505@gmail.com>
2022-03-02 13:04:50 +0100

    added the whitelist and pvp and removed defaults

--- src/ServerProperties.js
+++ src/ServerProperties.js
@@ -7,6 +7,8 @@
  * @property {Number=} maxPlayers=20
  * @property {Boolean=} enableCommandBlock=false
  * @property {String=} motd="A Minecraft Server"
+ * @property {Boolean=} whiteList=false
+ * @property {Boolean=} pvp=true
  * 
  */
 class ServerProperties {
@@ -21,12 +23,12 @@ class ServerProperties {
         return `
                 #Minecraft server properties
                 #Sun Feb 27 15:19:00 CET 2022
-                view-distance=${this.properties.viewDistance || 10}
-                max-build-height=${this.properties.maxBuildHeight || 256}
+                view-distance=${this.properties.viewDistance}
+                max-build-height=${this.properties.maxBuildHeight}
                 server-ip=
                 level-seed=
                 allow-nether=${Boolean(this.properties.allowNether)}
-                server-port=${this.properties.port || 25565}
+                server-port=${this.properties.port}
                 enable-command-block=${Boolean(this.properties.enableCommandBlock)}
                 gamemode=0
                 enable-rcon=false
@@ -40,8 +42,8 @@ class ServerProperties {
                 announce-player-achievements=true
                 force-gamemode=false
                 hardcore=false
-                white-list=false
-                pvp=true
+                white-list=${Boolean(this.properties.whiteList)}
+                pvp=${Boolean(this.properties.pvp)}
                 spawn-npcs=true
                 spawn-animals=true
                 generate-structures=true
@@ -50,7 +52,7 @@ class ServerProperties {
                 network-compression-threshold=256
                 level-type=DEFAULT
                 spawn-monsters=true
-                max-players=${this.properties.maxPlayers || 20}
+                max-players=${this.properties.maxPlayers}
                 spawn-protection=16
                 online-mode=true
                 allow-flight=false
