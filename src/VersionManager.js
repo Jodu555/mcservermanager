9cd60480e89e3092a494332d4b4d71eb4ea4f9ff
Jodu555 <Jodu505@gmail.com>
2022-03-04 12:21:02 +0100

    calculated the java version

--- src/VersionManager.js
+++ src/VersionManager.js
@@ -1,5 +1,6 @@
 const javaVersions = {
     8: '8.0.302-open',
+    17: '17.0.2-open'
 }
 
 class VersionManager {
@@ -9,8 +10,9 @@ class VersionManager {
 
     get(type = 'spigot', version = '1.18.1') {
         const effectiveVersion = version.split('.')[1];
-        const JavaVersion;
-        console.log(effectiveVersion);
+        const javaVersion = effectiveVersion > 16 ? javaVersions['17'] : javaVersions['8'];
+
+        console.log(version, effectiveVersion, javaVersion);
     }
 
 }
