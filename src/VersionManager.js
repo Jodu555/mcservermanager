e28b917014973f23ab1b08b70fe4ef3f1cb34371
Jodu555 <Jodu505@gmail.com>
2022-03-10 23:08:52 +0100

    started to implement the file download

--- src/VersionManager.js
+++ src/VersionManager.js
@@ -1,3 +1,8 @@
+const fs = require('fs');
+const axios = require('axios');
+
+
+
 const javaVersions = {
     8: '8.0.302-open',
     17: '17.0.2-open'
@@ -13,8 +18,38 @@ class VersionManager {
         const javaVersion = effectiveVersion > 16 ? javaVersions['17'] : javaVersions['8'];
 
         console.log(version, effectiveVersion, javaVersion);
+        return new Version(this, type, version, javaVersion)
     }
 
+}
+
+class Version {
+    constructor(versionmanager, type, version, javaVersion) {
+        this.versionmanager = versionmanager;
+        this.type = type;
+        this.version = version;
+        this.javaVersion = javaVersion;
+    }
+
+    download(url, path) {
+        const writer = fs.createWriteStream(path)
+        axios.get(url, { responseType: 'stream' }).then(response => {
+            return new Promise((resolve, reject) => {
+                response.data.pipe(writer);
+                let error = null;
+                writer.on('error', err => {
+                    error = err;
+                    writer.close();
+                    reject(err);
+                });
+                writer.on('close', () => {
+                    if (!error) {
+                        resolve(true);
+                    }
+                });
+            });
+        });
+    }
 }
 
 module.exports = VersionManager;
--- test.js
+++ test.js
@@ -1,31 +1,33 @@
 const MCServer = require("./src/MCServer");
-// const ServerProperties = require("./src/ServerProperties");
-// const VersionManager = require("./src/VersionManager");
+const ServerProperties = require("./src/ServerProperties");
+const VersionManager = require("./src/VersionManager");
 
 
 // // const props = new ServerProperties({ motd: 'Test123', allowFlight: true });
 // // console.log(props.out());
 
-// const veM = new VersionManager('test');
+const veM = new VersionManager('test');
 
-// veM.get('spigot', '1.18.1');
-// veM.get('spigot', '1.18');
-// veM.get('spigot', '1.17.1');
-// veM.get('spigot', '1.17');
-// veM.get('spigot', '1.16.5');
-// veM.get('spigot', '1.16.4');
-// veM.get('spigot', '1.16.3');
-// veM.get('spigot', '1.16.2');
-// veM.get('spigot', '1.16.1');
-// veM.get('spigot', '1.15.2');
-// veM.get('spigot', '1.15.1');
-// veM.get('spigot', '1.15');
+veM.get('spigot', '1.18.1');
+veM.get('spigot', '1.18');
+veM.get('spigot', '1.17.1');
+veM.get('spigot', '1.17');
+veM.get('spigot', '1.16.5');
+veM.get('spigot', '1.16.4');
+veM.get('spigot', '1.16.3');
+veM.get('spigot', '1.16.2');
+veM.get('spigot', '1.16.1');
+veM.get('spigot', '1.15.2');
+veM.get('spigot', '1.15.1');
+veM.get('spigot', '1.15');
 
-const server = new MCServer('Test123', undefined, {
-    port: 3737,
-})
 
-server.init();
+
+// const server = new MCServer('Test123', undefined, {
+//     port: 3737,
+// })
+
+// server.init();
 
 
 // createMCServerManager()
