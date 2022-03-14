5e5a5a9c660a75d2a7e29cc2a222d63efab90af0
Jodu555 <Jodu505@gmail.com>
2022-03-14 22:15:40 +0100

    Tested and working

--- test.js
+++ test.js
@@ -35,26 +35,7 @@ if (!isWin) {
 
 (async () => {
 
-    executeInteractiveCommand(process.cwd(), 'sdk');
-
-    // const { exec } = require('child_process');
-
-    // const ls = exec('cat ~/.bashrc', { shell: '/bin/bash' }, function (error, stdout, stderr) {
-    //     if (error) {
-    //         console.log(error.stack);
-    //         console.log('Error code: ' + error.code);
-    //         console.log('Signal received: ' + error.signal);
-    //     }
-    //     console.log('Child Process STDOUT: ' + stdout);
-    //     console.log('Child Process STDERR: ' + stderr);
-    // });
-
-    // ls.on('exit', function (code) {
-    //     console.log('Child process exited with exit code ' + code);
-    // });
-
-
-    // console.log(await executeCommand(process.cwd(), 'echo "$SHELL" && bash && sdk'));
+    console.log(await executeInteractiveCommand(process.cwd(), 'sdk'));
 
     return;
 
